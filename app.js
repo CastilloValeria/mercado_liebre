86c2a4126c23a729a5b9b03c95200fd2f144f14a
CastilloValeria <casvaleli87@gmail.com>
2023-10-24 17:55:48 -0300

    agregando cambios en home.html y en styles.css

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express= require("express");
 const path= require ("path");
-const port =3000;
+const puerto =3030;
 const app= express();
 
 app.listen(puerto, ()=>console.log(`server running in : http://localhost:${puerto}`));
