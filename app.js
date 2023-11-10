bdc31ae629cc680294fc48c7a67d6ba4dba05df8
CastilloValeria <casvaleli@gmail.com>
2023-11-10 00:00:24 -0300

    agregando archivos de formularios registro y loggin con sus respectivos css

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express= require("express");
 const path= require ("path");
 const puerto =3030;
 const app= express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.listen(puerto, ()=>console.log(`server running in : http://localhost:${puerto}`));
 
@@ -14,3 +17,22 @@ app.use (express.static("public"));
 app.get("/", (req, res)=>{
     res.sendFile(__dirname+"/view/home.html" );
 })
+app.get("/registro", (req, res)=>{
+    res.sendFile(__dirname+"/view/registre.html" );
+})
+
+app.get("/login", (req, res)=>{
+    res.sendFile(__dirname+"/view/login.html" );
+})
+
+app.post("/ingresar",( req,res)=>{
+    console.log(req.body);
+    res.redired( "/")
+})
+
+
+
+app.get("/ingresar", (req, res)=>{
+    res.sendFile(__dirname+"/view/login.html" );
+})
+
